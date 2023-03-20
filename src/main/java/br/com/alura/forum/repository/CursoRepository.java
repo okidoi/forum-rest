4dafdf7aab93e3acc18c16a2b3def262498b5a10
okidoi <okidoi@gmail.com>
2023-03-20 17:07:30 -0300

    Teste 2 de commit com Token

--- src/main/java/br/com/alura/forum/repository/CursoRepository.java
+++ src/main/java/br/com/alura/forum/repository/CursoRepository.java
@@ -6,6 +6,6 @@ import br.com.alura.forum.modelo.Curso;
 
 public interface CursoRepository extends JpaRepository<Curso, Long> {
 
-	Curso findByNome(String nome);
+	Curso findByNome(String nome);  //Teste de commit
 
 }
